8e7fc475de3274318a9e8efc988f10de5c7cd9be
Floriab242 <floriancata04@gmail.com>
2020-09-21 09:12:42 +1100

    ajout boutton annuler dans "login"

--- view/login.php
+++ view/login.php
@@ -1,6 +1,6 @@
 <?php $title= 'Exemple Annonces Basic PHP: Connexion'; ?>
 <?php ob_start(); ?>
-<div class="log">
+< class="log">
     <form method="post" action="/TAP/index.php/annonces.php">
         <label for="login"> Votre identifiant </label> :
         <input type="text" name="login" id="login" placeholder="defaut" maxlength="12" required />
@@ -9,7 +9,9 @@
         <input type="password" name="password" id="password" maxlength="12" required />
         <input type="submit" value="Envoyer">
     </form>
-    <a href="/TAP/index.php/register.php" class="acount"></a>
+    <form action="/TAP/index.php">
+        <input type="submit" value="Annuler" />
+    </form>
 </div>
 <?php $content = ob_get_clean(); ?>
 <?php include 'layout.php'; ?>
