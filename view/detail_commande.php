fce739f7fa9b5e54de00fb6aa44a622a59c97721
filitika <keren.filitika@gmail.com>
2020-10-06 10:13:17 +1100

    Bouton retour dans detail_commande.php

--- view/detail_commande.php
+++ view/detail_commande.php
@@ -2,9 +2,11 @@
 
 <?php ob_start(); ?>
 
-        <h1> Commande <?php echo $id ; ?> </h1>
+        <h1> Commande <?php echo $post['id_commande']  ; ?> </h1>
 
-        <div class="log"> Details: <?php echo $post['details'] ; ?> </div>
+        <div class="log"> Detail de la commande: <br/> <?php echo $post['details'] ; ?> </div>
+
+        <a href="/TAP/index.php/Liste_commande">Retour</a>
 
 <?php $content = ob_get_clean(); ?>
 
